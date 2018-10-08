f551c86f5ffff0e702893b7ffa5a89c415cee652
brucemoran <brucemoran@Bruces-iMac-2.local>
2018-10-08 10:39:06 +0100

    Add prerequisites.R

---
+++ scripts/prerequisites.R
@@ -0,0 +1,18 @@
+#! R
+##install prerequisite libraries
+libs <- c("RnBeads.hg19","RnBeads","RPMM","tidyverse","magrittr",	"pheatmap","mgcv","readxl","GenomicRanges","reshape2","ggplot2","VennDiagram")
+
+##install the great installer
+install.packages("BiocManager",
+                  repos="https://cloud.r-project.org",
+                  dependencies=TRUE)
+library("BiocManager")
+
+##install libs through BiocManager
+lapply(libs, function(f){
+  BiocManager::install(f,
+                       site_repository="https://cloud.r-project.org",
+                       ask=FALSE,
+                       update=TRUE,
+                       dependencies=TRUE)
+  })
